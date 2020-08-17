9597c5a191186081fc35bf2d9fc0426e3f948066
Silaeva <tkachenko481@mail.ru>
2020-08-17 22:20:05 +0300

    Добавляет абстрактный класс и презентер

--- src/const.js
+++ src/const.js
@@ -8,11 +8,5 @@ const COLORS = [
 const MAX_DAYS_GAP = 7;
 
 const TASK_COUNT = 22;
-const TASK_COUNT_PER_STEP = 8;
 
-const RenderPosition = {
-  AFTERBEGIN: `afterbegin`,
-  BEFOREEND: `beforeend`
-};
-
-export {COLORS, MAX_DAYS_GAP, TASK_COUNT, TASK_COUNT_PER_STEP, RenderPosition};
+export {COLORS, MAX_DAYS_GAP, TASK_COUNT};
